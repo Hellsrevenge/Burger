3303214ffdb8a3b805f4b4763cfdb9680f307b45
Hellsrevenge <ivark26@gmail.com>
2018-12-21 23:19:30 -0800

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,9 @@ CREATE DATABASE `burgers_db`;
 USE `burgers_db`;
 
 CREATE TABLE `burgers` (
-    id INT NOT NULL AUTO_INCREMENT,
+    burger_id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN NOT NULL,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`burger_id`)
 );
