70f651a909afc4f0ed9d51c154e01c0cbfab832e
Hellsrevenge <ivark26@gmail.com>
2018-12-22 15:53:19 -0800

    update config

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.CLEARDB_DATABASE_URL,
     port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
+    user: process.env.CLEARDB_USER,
+    password: process.env.CLEARDB_PASSWORD,
+    database: process.env.CLEARDB_NAME
 });
 
 module.exports = connection;
