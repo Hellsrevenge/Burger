7c3082f6995bb468ad0162719725b32276f5f0ca
Hellsrevenge <ivark26@gmail.com>
2018-12-22 16:30:19 -0800

    update config

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var config = {
     host: process.env.CLEARDB_DATABASE_HOST,
     port: 3306,
     user: process.env.CLEARDB_USER,
     password: process.env.CLEARDB_PASSWORD,
     database: process.env.CLEARDB_NAME
-});
+};
+
+var connection = mysql.createConnection(config);
 
 connection.connect(function(err) {
     if (err) {
         console.log(err);
+        console.log(config);
         return;
     }
 });
